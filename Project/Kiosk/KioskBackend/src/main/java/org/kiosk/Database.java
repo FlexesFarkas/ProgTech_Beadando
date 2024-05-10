b652180ed1b7bfcc5922ad6bca75f9f6ffe437cc
Cz. Tóth Csanád <135127015+SapiS68@users.noreply.github.com>
2024-05-10 19:19:33 +0200

    Adatbázis és generálás refaktorálás

--- Project/Kiosk/KioskBackend/src/main/java/org/kiosk/Database.java
+++ Project/Kiosk/KioskBackend/src/main/java/org/kiosk/Database.java
@@ -30,37 +30,54 @@ public class Database {
             System.out.println("Database found!");
         }
         else {
-            try{
-                System.out.println("Database not found! Creating a new database...");
-                Connection dbConenction = DriverManager.getConnection(CONNECTION_URL);
-                Statement statement = dbConenction.createStatement();
-                String sql = "BEGIN TRANSACTION;\n" +
-                        "CREATE TABLE IF NOT EXISTS \"Ingredients\" (\n" +
-                        "\t\"ingredient_id\"\tINTEGER NOT NULL UNIQUE,\n" +
-                        "\t\"ingredient_name\"\tTEXT NOT NULL UNIQUE,\n" +
-                        "\t\"ingredient_ammount\"\tINTEGER NOT NULL,\n" +
-                        "\t\"ingredient_price\"\tINTEGER NOT NULL,\n" +
-                        "\tPRIMARY KEY(\"ingredient_id\" AUTOINCREMENT)\n" +
-                        ");\n" +
-                        "CREATE TABLE IF NOT EXISTS \"Burgers\" (\n" +
-                        "\t\"burger_id\"\tTEXT NOT NULL,\n" +
-                        "\t\"burger_ingredients\"\tINTEGER NOT NULL,\n" +
-                        "\t\"burger_base_price\"\tINTEGER NOT NULL,\n" +
-                        "\tPRIMARY KEY(\"burger_id\")\n" +
-                        ");\n" +
-                        "CREATE TABLE IF NOT EXISTS \"Orders\" (\n" +
-                        "\t\"order_id\"\tTEXT NOT NULL,\n" +
-                        "\t\"burger_id\"\tTEXT NOT NULL,\n" +
-                        "\tFOREIGN KEY(\"burger_id\") REFERENCES \"Burgers\"(\"burger_id\"),\n" +
-                        "\tPRIMARY KEY(\"order_id\",\"burger_id\")\n" +
-                        ");\n" +
-                        "COMMIT;\n";
-                statement.executeLargeUpdate(sql);
-                dbConenction.close();
-                System.out.println("New database successfully created!");
-            } catch(Exception e){
-                System.err.println("Failed to create database: " + e.getMessage());
-            }
+            createDatabase();
+        }
+    }
+
+    private static void createDatabase() {
+        try{
+            System.out.println("Database not found! Creating a new database...");
+            Connection dbConenction = DriverManager.getConnection(CONNECTION_URL);
+            Statement statement = dbConenction.createStatement();
+            String sql = "BEGIN TRANSACTION;\n" +
+                    "CREATE TABLE IF NOT EXISTS \"BurgerIngredients\" (\n" +
+                    "\t\"burger_ingredient_id\"\tINTEGER NOT NULL UNIQUE,\n" +
+                    "\t\"burger_ingredient_name\"\tTEXT NOT NULL UNIQUE,\n" +
+                    "\t\"burger_ingredient_amount\"\tINTEGER NOT NULL,\n" +
+                    "\t\"burger_ingredient_price\"\tINTEGER NOT NULL,\n" +
+                    "\tPRIMARY KEY(\"burger_ingredient_id\" AUTOINCREMENT)\n" +
+                    ");\n" +
+                    "CREATE TABLE IF NOT EXISTS \"ShakeIngredients\" (\n" +
+                    "\t\"shake_ingredient_id\"\tINTEGER NOT NULL UNIQUE,\n" +
+                    "\t\"shake_ingredient_name\"\tTEXT NOT NULL UNIQUE,\n" +
+                    "\t\"shake_ingredient_amount\"\tINTEGER NOT NULL,\n" +
+                    "\t\"shake_ingredient_price\"\tINTEGER NOT NULL,\n" +
+                    "\tPRIMARY KEY(\"shake_ingredient_id\" AUTOINCREMENT)\n" +
+                    ");\n" +
+                    "CREATE TABLE IF NOT EXISTS \"Burgers\" (\n" +
+                    "\t\"burger_id\"\tTEXT NOT NULL,\n" +
+                    "\t\"burger_ingredients\"\tINTEGER NOT NULL,\n" +
+                    "\tPRIMARY KEY(\"burger_id\")\n" +
+                    ");\n" +
+                    "CREATE TABLE IF NOT EXISTS \"Shakes\" (\n" +
+                    "\t\"shake_id\"\tTEXT NOT NULL,\n" +
+                    "\t\"shake_ingredients\"\tINTEGER NOT NULL,\n" +
+                    "\tPRIMARY KEY(\"shake_id\")\n" +
+                    ");\n" +
+                    "CREATE TABLE IF NOT EXISTS \"Orders\" (\n" +
+                    "\t\"order_id\"\tTEXT NOT NULL,\n" +
+                    "\t\"burger_id\"\tTEXT NOT NULL,\n" +
+                    "\t\"shake_id\"\tTEXT NOT NULL,\n" +
+                    "\tFOREIGN KEY(\"burger_id\") REFERENCES \"Burgers\"(\"burger_id\"),\n" +
+                    "\tFOREIGN KEY(\"shake_id\") REFERENCES \"Shakes\"(\"shake_id\"),\n" +
+                    "\tPRIMARY KEY(\"order_id\",\"burger_id\",\"shake_id\")\n" +
+                    ");\n" +
+                    "COMMIT;\n";
+            statement.executeLargeUpdate(sql);
+            dbConenction.close();
+            System.out.println("New database successfully created!");
+        } catch(Exception e){
+            System.err.println("Failed to create database: " + e.getMessage());
         }
     }
 }
