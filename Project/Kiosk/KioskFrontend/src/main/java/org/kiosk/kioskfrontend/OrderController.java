2e53c2ae25ac4c8a36dfd351ce63ed690dd468c8
FlexesFarkas <detarilevi@gmail.com>
2024-05-21 10:28:50 +0200

    kezdetleges rendelési felület elkészítve

---
+++ Project/Kiosk/KioskFrontend/src/main/java/org/kiosk/kioskfrontend/OrderController.java
@@ -0,0 +1,51 @@
+package org.kiosk.kioskfrontend;
+
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+
+public class OrderController {
+    public void Food1_Selected(MouseEvent mouseEvent) {
+    }
+
+    public void FoodButtonColorChange(MouseDragEvent mouseDragEvent) {
+    }
+
+    public void Food2_Selected(MouseEvent mouseEvent) {
+    }
+
+    public void Food3_Selected(MouseEvent mouseEvent) {
+    }
+
+    public void Food4_Selected(MouseEvent mouseEvent) {
+    }
+
+    public void Topping1_minus(MouseEvent mouseEvent) {
+    }
+
+    public void Topping2_minus(MouseEvent mouseEvent) {
+    }
+
+    public void Topping3_minus(MouseEvent mouseEvent) {
+    }
+
+    public void Topping4_minus(MouseEvent mouseEvent) {
+    }
+
+    public void Topping5_minus(MouseEvent mouseEvent) {
+    }
+
+    public void Topping1_plus(MouseEvent mouseEvent) {
+    }
+
+    public void Topping2_plus(MouseEvent mouseEvent) {
+    }
+
+    public void Topping3_plus(MouseEvent mouseEvent) {
+    }
+
+    public void Topping4_plus(MouseEvent mouseEvent) {
+    }
+
+    public void Topping5_plus(MouseEvent mouseEvent) {
+    }
+}
